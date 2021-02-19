0bb3b177ad122e98687a5a6d6618c9a66dff10c6
unknown <ante.dujic0506@gmail.com>
2021-02-19 18:26:20 +0000

    Week format?

--- daytime.py
+++ daytime.py
@@ -1,6 +1,25 @@
 
+# https://www.w3resource.com/python-exercises/date-time-exercise/python-date-time-exercise-1.php
+# https://www.w3schools.com/python/python_datetime.asp
 
-import time
 import datetime
 
-print("Day of week: ", datetime.date.today().strftime("%A"))
+today = datetime.datetime.now()
+
+
+print (today.strftime("%u"))
+
+
+if (int ((today.strftime("%u"))) > 5):
+    print ("Today is the weekend, yay!")
+else:
+    print ("Yes, unfortunately today is a weekday. :( ")
+
+print (today.strftime("%w"))
+
+if (int ((today.strftime("%w"))) > 5):
+    print ("Today is the weekend, yay!")
+else:
+    print ("Yes, unfortunately today is a weekday. :( ")
+    
+    
