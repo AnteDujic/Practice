f0f4695166b22eff3e723dd31b7641e763738a3e
unknown <ante.dujic0506@gmail.com>
2021-03-13 11:19:46 +0000

    Reasearch for week07 task

--- arguments.py
+++ arguments.py
@@ -12,3 +12,5 @@ print("Arguments of the script", sys.argv[1:])
 # https://bham-carpentries.github.io/2018-07-26-python_python-novice-inflammation/10-cmdline/index.html
 
 # https://stackoverflow.com/questions/4117530/sys-argv1-meaning-in-script
+
+# https://askubuntu.com/questions/1059579/input-the-filename-in-the-commandline-as-an-argument-in-python
